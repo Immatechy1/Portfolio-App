eff9cda54905dfe703ee8195a348eb91c322cbb8
Immaculeta Nwokejeme <immaculetanwokejeme@gmail.com>
2023-04-22 13:50:45 +0100

    first commit

---
+++ src/componentData/Navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+
+const Navbar = () => {
+    return (
+        <>
+            <h1 className="p-10 bg-black text-white text-24 font-bold underline">
+                Hello world!
+            </h1>
+           
+        </>   
+    );
+  };
+
+export default Navbar;
+  
+
+
+  
---
+++ src/components/Layout/BasePageLayout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Navbar from "@/src/componentData/Navbar/NavBar";
+
+const BasePageLayout = ({ }) => {
+    return (
+      <div>
+        <Navbar/>
+      </div>
+    );
+  };
+
+export default BasePageLayout;
+  
